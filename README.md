2c9c33e40d15fcbde0796a4f8f97ecdd36ed2598
sprintrace <86525084+sprintrace@users.noreply.github.com>
2024-01-18 14:15:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Toronto-Bike-Theft
+ Repository for exploring bike theft data in Toronto
---
+++ scripts/01-download_data.R
@@ -0,0 +1,20 @@
+library(opendatatoronto)
+library(dplyr)
+
+# get package
+package <- show_package("neighbourhood-crime-rates")
+package
+
+# get all resources for this package
+resources <- list_package_resources("neighbourhood-crime-rates")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) |> get_resource()
+data
+
+
+
+write.csv(data, "inputs/data/biketheftdata.csv", row.names=FALSE) ## save to CSV format
